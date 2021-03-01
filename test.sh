b6b892eab33b1c258e4fc557b5e784024ba85573
alph97 <47526291+alph97@users.noreply.github.com>
2021-03-01 08:25:24 +0100

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+. functions
+
+hello
+hello_name Toto
+exist text.txt test
+add_line text.txt ajouter
+replace_line text.txt voila oui
+replace_line text.txt test
+save_files text.txt
