0b24a6f5496793a0ac2b3d4a3ae73043e58e5534
rodneyAboue <62360031+rodneyAboue@users.noreply.github.com>
2021-03-03 10:03:11 +0100

    Add files via upload

--- set_nfs.sh
+++ set_nfs.sh
@@ -2,20 +2,29 @@
 . functions
 . param
 
-ma_machine=hostname -a 
+#ma_machine=hostname -a 
 
-if [ $ma_machine eq "serveur" ]; then 
-	echo "machine serveur"
-	systemctl enable nfs-server
-	systemctl start nfs-server
-	systemctl stop firewalld.service
-	mkdir -p /export/home 
-	mkdir -p /export/opt 
-	
+#if [ $ma_machine -eq "serveur" ]; then 
+	#echo "machine serveur"
+#	systemctl enable nfs-server
+#	systemctl start nfs-server
+#	systemctl stop firewalld.service
+	cheminopt=/export/opt
+	cheminhome=/export/home
+	#mkdir -p $cheminhome
+	#mkdir -p $cheminopt 
+	#mise a jour 
 
-
-else
-	echo "machine non serveur"
-fi
-
-
+		IFS=$'\n'
+		#echo $( cat $MACHINE_LISTE )
+		echo "------------------------------"
+		nom=$( cut -d ' ' -f2 $MACHINE_LISTE  )
+        #echo $nom
+        for a in $nom
+    	do
+        	echo "$cheminopt $a()"
+    	done 
+    	
+#else
+#	echo "machine non serveur"
+#fi
